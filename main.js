a3e5cfac98bf758b68b6f3719ef460fde11f6f88
Muyao Xu <muyaoxu@Muyaos-MacBook-Pro.local>
2018-09-11 19:53:17 -0700

    finished

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+let postTemplateString = document.getElementById('post-template').innerHTML;
+let renderPost = Handlebars.compile(postTemplateString);
+
+Handlebars.registerHelper('formate-value', function(value) {
+  return value.toLocaleString();
+});
+
+Handlebars.registerHelper('relative-date', function(date) {
+	return moment(date).fromNow();
+});
+
 $('#search-form').on('submit', function(event) {
 	event.preventDefault();
 	let subreddit = $('#search-form').serializeArray()[0].value;
@@ -11,23 +22,37 @@ $('#search-form').on('submit', function(event) {
 	});
 
 	promise.then(function(posts) {
-		let html = '';
-		// console.log(posts.data.children[0]);
-		var searchResults = posts.data.children;
-		// console.log(searchResults);
-		searchResults.forEach(function(post) {
-			// console.log(post.data.title);
-			html += `<p>title: ${post.data.title}, score: ${post.data.score}, 
-					author: ${post.data.author}</p>`;
-			// console.log(html);
-		});
-		
-		// html = posts.data.children;
 		$('.loader').hide();
-		$('#results').html(html);
+		console.log(posts.data.children);
+		let renderedPost = renderPost({
+			posts: posts.data.children
+		});
+		// console.log(renderedPost);
+		$('body').append(renderedPost);
+	}, function() {
+		$('.loader').hide();
+		error_msg = "Oops! Something went wrong!"
+		$('body').append(error_msg);
+	})
 
-		// $('#resulsts').html(html);
-	}, function(error) {
-		console.log('error', error);
-	});
+	// promise.then(function(posts) {
+	// 	let html = '';
+	// 	// console.log(posts.data.children[0]);
+	// 	var searchResults = posts.data.children;
+	// 	// console.log(searchResults);
+	// 	searchResults.forEach(function(post) {
+	// 		// console.log(post.data.title);
+	// 		html += `<p>title: ${post.data.title}, score: ${post.data.score}, 
+	// 				author: ${post.data.author}</p>`;
+	// 		// console.log(html);
+	// 	});
+		
+	// 	// html = posts.data.children;
+	// 	$('.loader').hide();
+	// 	$('#results').html(html);
+
+	// 	// $('#resulsts').html(html);
+	// }, function(error) {
+	// 	console.log('error', error);
+	// });
 });

643a014ba7e51a6c3900666aa3f8d39ee2e2b5ee
Muyao Xu <muyaoxu@Muyaos-MacBook-Pro.local>
2018-09-11 19:56:56 -0700

    Add score to div

--- main.js
+++ main.js
@@ -5,10 +5,6 @@ Handlebars.registerHelper('formate-value', function(value) {
   return value.toLocaleString();
 });
 
-Handlebars.registerHelper('relative-date', function(date) {
-	return moment(date).fromNow();
-});
-
 $('#search-form').on('submit', function(event) {
 	event.preventDefault();
 	let subreddit = $('#search-form').serializeArray()[0].value;
@@ -27,7 +23,6 @@ $('#search-form').on('submit', function(event) {
 		let renderedPost = renderPost({
 			posts: posts.data.children
 		});
-		// console.log(renderedPost);
 		$('body').append(renderedPost);
 	}, function() {
 		$('.loader').hide();
@@ -35,24 +30,4 @@ $('#search-form').on('submit', function(event) {
 		$('body').append(error_msg);
 	})
 
-	// promise.then(function(posts) {
-	// 	let html = '';
-	// 	// console.log(posts.data.children[0]);
-	// 	var searchResults = posts.data.children;
-	// 	// console.log(searchResults);
-	// 	searchResults.forEach(function(post) {
-	// 		// console.log(post.data.title);
-	// 		html += `<p>title: ${post.data.title}, score: ${post.data.score}, 
-	// 				author: ${post.data.author}</p>`;
-	// 		// console.log(html);
-	// 	});
-		
-	// 	// html = posts.data.children;
-	// 	$('.loader').hide();
-	// 	$('#results').html(html);
-
-	// 	// $('#resulsts').html(html);
-	// }, function(error) {
-	// 	console.log('error', error);
-	// });
 });
